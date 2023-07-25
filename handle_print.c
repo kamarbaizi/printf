306781d63e85283b5a60499038207680948f8e40
oumasabrin <oumaimaaloui001@gmail.com>
2023-07-25 12:52:55 -0700

    update

--- handle_print.c
+++ handle_print.c
@@ -11,11 +11,16 @@
  * @size: Size specifier
  * Return: 1 or 2;
 */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list,
+	char buffer[], int flags, int width, int precision, int size)
 {
 int i, unknow_len = 0, printed_chars = -1;
 fmt_t fmt_types[] = {
-{'c', print_char}, {'s', print_string}, {'%', print_percent}, {'i', print_int}, {'d', print_int}, {'b', print_binary}, {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal}, {'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable}, {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	{'c', print_char}, {'s', print_string}, {'%', print_percent},
+	{'i', print_int}, {'d', print_int}, {'b', print_binary},
+	{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+	{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+	{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 };
 for (i = 0; fmt_types[i].fmt != '\0'; i++)
 if (fmt[*ind] == fmt_types[i].fmt)
