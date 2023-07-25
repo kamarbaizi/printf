0565c558640b7cebb7870f524755e1f71014defe
oumasabrin <oumaimaaloui001@gmail.com>
2023-07-25 14:13:47 -0700

    updated

--- functions1.c
+++ functions1.c
@@ -59,7 +59,7 @@ buffer[i--] = (num % 8) + '0';
 num /= 8;
 }
 
-if (flags & F_HASH && init_num != 0)
+if (flags && F_HASH && init_num != 0)
 buffer[i--] = '0';
 i++;
 return (write_unsgnd(0, i, buffer, flags, width, precision, size));
