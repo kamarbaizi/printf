8cb7a520514861d55bc4a9ab89efaf3bb1b4d4bd
oumasabrin <oumaimaaloui001@gmail.com>
2023-07-25 13:39:46 -0700

    update

--- functions.c
+++ functions.c
@@ -153,7 +153,7 @@ int print_binary(va_list types, char buffer[], int flags,
 	unsigned int a[32];
 	int count;
 
-	UNUSED{buffer}
+	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
@@ -167,7 +167,8 @@ int print_binary(va_list types, char buffer[], int flags,
 	m /= 2;
 	a[i] = (n / m) % 2;
 }
-for (i = 0; sum = 0, count = 0; i < 32; i++)
+for (i = 0; sum = 0, count = 0; i < 32)
+ i++;
 {
 	sum += a[i];
 	if (sum || i == 31)
